28a67d29d1b0f787c36e5cda5386717edec26263
CatalinID <ionut.dondera@student.upt.ro>
2018-11-04 18:16:58 +0200

    Added side drawer
    MADE- side drawer
    FOR WHAT- Home, My appointments, Settings drawers
    WIWD- Login/Register screen at first app launch

--- app/src/main/java/com/msaproject/catal/myappointment/MainPage.java
+++ app/src/main/java/com/msaproject/catal/myappointment/MainPage.java
@@ -1,8 +1,18 @@
 package com.msaproject.catal.myappointment;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import com.mikepenz.materialdrawer.*;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 public class MainPage extends AppCompatActivity {
 
@@ -12,5 +22,58 @@ public class MainPage extends AppCompatActivity {
         setContentView(R.layout.activity_main_page);
         TextView textView=findViewById(R.id.textUserName);
         textView.setText("Welcome User!");
+
+        AccountHeader accountHeader = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withCompactStyle(true)
+                .withTranslucentStatusBar(true)
+                .withHeaderBackground(R.color.md_dark_background)
+                .addProfiles(
+                        new ProfileDrawerItem().withName("Catalin Dondera").withEmail("ionut.dondera@student.upt.ro").withIcon(R.drawable.avatar)
+                )
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean current) {
+                        return false;
+                    }
+                })
+                .withCloseDrawerOnProfileListClick(true)
+                .build();
+
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withName("Home")
+                .withTextColor(Color.LTGRAY)
+                .withSelectedColor(Color.LTGRAY)
+                .withSelectedTextColor(Color.BLACK)
+                .withIcon(R.drawable.home);
+        PrimaryDrawerItem item2 = new PrimaryDrawerItem().withName("My Appointments")
+                .withTextColor(Color.LTGRAY)
+                .withSelectedColor(Color.LTGRAY)
+                .withSelectedTextColor(Color.BLACK)
+                .withIcon(R.drawable.calendar);
+        PrimaryDrawerItem item3 = new PrimaryDrawerItem().withName("Settings")
+                .withTextColor(Color.LTGRAY)
+                .withSelectedColor(Color.LTGRAY)
+                .withSelectedTextColor(Color.BLACK)
+                .withIcon(R.drawable.black_settings_button);
+
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withAccountHeader(accountHeader)
+                .addDrawerItems(
+                        item1,
+                        new DividerDrawerItem(),
+                        item2,
+                        new DividerDrawerItem(),
+                        item3
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        return false;
+                    }
+                })
+                .withSliderBackgroundColor(Color.DKGRAY)
+                .withCloseOnClick(true)
+                .build();
     }
 }
